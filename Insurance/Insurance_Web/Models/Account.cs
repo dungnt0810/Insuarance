a576fbbf6a05db193ffb0a968a2b70f178ed2847
Dung <dung.nguyen.tien.0810@gmail.com>
2020-07-14 17:15:34 +0700

    Add model account + Role

---
+++ Insurance/Insurance_Web/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Insurance_Web.Models
+{
+    public partial class Account
+    {
+        public int IdAccount { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Image { get; set; }
+        public bool? Status { get; set; }
+        public int IdRole { get; set; }
+    }
+}
