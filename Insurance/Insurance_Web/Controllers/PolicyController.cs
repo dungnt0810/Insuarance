a2435a38ea39f6e194afcaff79e4883e30b0bee8
Tran <dangtranthd@gmaomil.c>
2020-07-16 20:00:19 +0700

    test template

---
+++ Insurance/Insurance_Web/Controllers/PolicyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Insurance_Web.Controllers
+{
+    [Route("policy")]
+    public class PolicyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
